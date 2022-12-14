460dbe7dd39ce748dd3ca9c98e6314b055c720c5
Greg Troszak <greg@isentropic.dev>
2022-12-14 06:59:43 -0500

    Matrix inversion with nalgebra (#4)

---
+++ examples/matrix_inversion.rs
@@ -0,0 +1,18 @@
+use na::Matrix4;
+
+fn main() {
+    let invertible = Matrix4::new(
+        5., 6., 6., 8., 2., 2., 2., 8., 6., 6., 2., 8., 2., 3., 6., 7.,
+    );
+
+    let not_invertible = Matrix4::zeros();
+
+    for matrix in [invertible, not_invertible] {
+        if matrix.is_invertible() {
+            println!("{:?} is invertible.", matrix);
+            println!("Inverse: {:?}", matrix.try_inverse().unwrap());
+        } else {
+            println!("{:?} is not invertible.", matrix);
+        }
+    }
+}
