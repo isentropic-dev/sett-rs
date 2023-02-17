21faf0356301c179d7d474412b2ee8d3f71d3752
John Dyreby <john@isentropic.dev>
2023-02-17 09:02:42 -0600

    Clean up ws module

--- src/ws.rs
+++ src/ws.rs
@@ -1,35 +1,42 @@
-// mod gpu3;
-// mod mod2;
-// mod rhombic_drive;
+mod gpu3;
+mod mod2;
+mod rhombic_drive;
 pub mod sinusoidal_drive;
 
-use crate::types::{Environment, ParasiticPower};
+// Export all available working spaces components
+pub use gpu3::GPU3;
+pub use mod2::Mod2;
+pub use rhombic_drive::RhombicDrive;
+pub use sinusoidal_drive::SinusoidalDrive;
+
+use crate::{engine::state::Pressure as EnginePressure, types::ParasiticPower};
 
 pub trait WorkingSpaces {
     /// Returns the frequency (Hz) of the engine
     fn frequency(&self, state: &State) -> f64;
 
-    /// Returns a function that provides `Volumes` as function of time
-    fn volumes(&self, state: &State) -> Box<dyn Fn(f64) -> Volumes>;
+    /// Returns a function for `CompressionVolume` and `ExpansionVolume` as a function of time
+    fn volumes(&self, state: &State) -> Box<dyn Fn(f64) -> (CompVolume, ExpVolume)>;
 
     /// Returns the thermal resistances of the compression and expansion spaces
     fn thermal_resistance(&self, state: &State) -> ThermalResistance;
 
     /// Returns the parasitic power associated with the working spaces
     fn parasitics(&self, state: &State) -> Parasitics;
-
-    /// Returns information about the working spaces model
-    fn report(&self, state: &State) -> String;
 }
 
-/// Volumes (m^3) and their derivatives (m^3/s) of the two spaces
-#[allow(non_snake_case)]
+/// Compression space volume (m^3) and its derivative (m^3/s)
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Volumes {
-    pub V_c: f64,
-    pub V_e: f64,
-    pub dVc_dt: f64,
-    pub dVe_dt: f64,
+pub struct CompVolume {
+    pub value: f64,
+    pub deriv: f64,
+}
+
+/// Expansion space volume (m^3) and its derivative (m^3/s)
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct ExpVolume {
+    pub value: f64,
+    pub deriv: f64,
 }
 
 /// Thermal resistance between the gas and the associated heat exchanger temperature
@@ -59,29 +66,7 @@ pub struct Parasitics {
     pub exp: ParasiticPower,
 }
 
-/// Information available to a component for calculating cycle parameters
-#[derive(Debug, Clone, Copy)]
+/// Information available to a ws component for calculating its parameters
 pub struct State {
-    pub env: Environment,
-    pub comp: Average,
-    pub exp: Average,
-}
-
-impl State {
-    /// Create a `ws::State` from sink and source temperatures
-    pub fn new(sink: f64, source: f64) -> Self {
-        Self {
-            env: Environment {
-                sink_temp: sink,
-                source_temp: source,
-            },
-            comp: Average {},
-            exp: Average {},
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct Average {
-    // as needed...
+    pub pres: EnginePressure,
 }
