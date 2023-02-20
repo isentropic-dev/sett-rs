3d42c84157c05da8b488dcc230f879cd0c0cfc62
John Dyreby <john@isentropic.dev>
2023-02-20 14:08:03 -0600

    Implement ideal gas equations (#26)

--- src/fluid.rs
+++ src/fluid.rs
@@ -5,4 +5,80 @@ mod refprop;
 // Export all available fluid models
 pub use ideal_gas::IdealGas;
 
-pub trait Fluid {}
+pub trait Fluid {
+    /// Return density in kg/m3
+    ///
+    /// # Arguments
+    ///
+    /// * `temp` - temperature (K)
+    /// * `pres` - pressure (Pa)
+    ///
+    fn dens(&self, temp: f64, pres: f64) -> f64;
+
+    /// Return specific internal energy in J/kg
+    ///
+    /// # Arguments
+    ///
+    /// * `temp` - temperature (K)
+    /// * `pres` - pressure (Pa)
+    ///
+    fn inte(&self, temp: f64, pres: f64) -> f64;
+
+    /// Return specific enthalpy in J/kg
+    ///
+    /// # Arguments
+    ///
+    /// * `temp` - temperature (K)
+    /// * `pres` - pressure (Pa)
+    ///
+    fn enth(&self, temp: f64, pres: f64) -> f64;
+
+    /// Return specific heat at constant pressure in J/kg-K
+    ///
+    /// # Arguments
+    ///
+    /// * `temp` - temperature (K)
+    /// * `pres` - pressure (Pa)
+    ///
+    fn cp(&self, temp: f64, pres: f64) -> f64;
+
+    /// Return derivative of density with respect to pressure at constant temperature in kg/m3-Pa
+    ///
+    /// # Arguments
+    ///
+    /// * `temp` - temperature (K)
+    /// * `pres` - pressure (Pa)
+    ///
+    #[allow(non_snake_case)]
+    fn dd_dP_T(&self, temp: f64, pres: f64) -> f64;
+
+    /// Return derivative of density with respect to temperature at constant pressure in kg/m3-K
+    ///
+    /// # Arguments
+    ///
+    /// * `temp` - temperature (K)
+    /// * `pres` - pressure (Pa)
+    ///
+    #[allow(non_snake_case)]
+    fn dd_dT_P(&self, temp: f64, pres: f64) -> f64;
+
+    /// Return derivative of internal energy with respect to pressure at constant temperature in J/kg-Pa
+    ///
+    /// # Arguments
+    ///
+    /// * `temp` - temperature (K)
+    /// * `pres` - pressure (Pa)
+    ///
+    #[allow(non_snake_case)]
+    fn du_dP_T(&self, temp: f64, pres: f64) -> f64;
+
+    /// Return derivative of internal energy with respect to temperature at constant pressure in J/kg-K
+    ///
+    /// # Arguments
+    ///
+    /// * `temp` - temperature (K)
+    /// * `pres` - pressure (Pa)
+    ///
+    #[allow(non_snake_case)]
+    fn du_dT_P(&self, temp: f64, pres: f64) -> f64;
+}
