2fa7b31b190aeb956350603713f0742c369b7bf2
Greg Troszak <gdtroszak@gmail.com>
2023-03-08 11:33:52 -0500

    Finish up regen.

--- src/regen/mod2.rs
+++ src/regen/mod2.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
 
-use crate::types::ParasiticPower;
+use crate::types::{Material, ParasiticPower};
 
 use super::{
     types::{FrictionFactorCorrelation, JFactorCorrelation},
@@ -18,12 +18,35 @@ pub struct Config {
     correlationj: JFactorCorrelation,
 }
 
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct Geometry {
     mesh: Mesh,
     shell: Shell,
 }
 
-pub enum Correlation {}
+#[allow(non_snake_case)]
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Mesh {
+    material: Material,
+    D_wire: f64,
+    pitch: f64,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Shell {
+    material: Material,
+    R_sh: f64,
+    th_sh_cold: f64,
+    th_sh_hot: f64,
+    length: f64,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub enum Correlation {
+    Steady,
+    Oscillating,
+}
 
 impl super::Regenerator for Mod2 {
     fn volume(&self) -> f64 {
