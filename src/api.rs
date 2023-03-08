91370bb02f228865e39133fdad69f71788db1c48
John Dyreby <john@isentropic.dev>
2023-03-08 12:41:31 -0600

    Move values into their own struct

--- src/api.rs
+++ src/api.rs
@@ -1,17 +1,71 @@
 use crate::engine::state;
 
 /// The results of an engine run
+pub struct RunResults {
+    /// Engine pressure (Pa)
+    pub pres: state::Pressure,
+
+    /// Engine temperatures (K)
+    pub temp: state::Temperatures,
+
+    /// Average mass flow rates through the heat exchangers (kg/s)
+    pub mass_flow: state::MassFlows,
+
+    /// Average heat flow rates through the heat exhangers (W)
+    pub heat_flow: state::HeatFlows,
+
+    /// Regenerator approach temperature imbalance (K)
+    pub regen_imbalance: state::RegenImbalance,
+
+    /// Time-discretized values over one engine cycle
+    pub values: Values,
+
+    /// Total heat input to the engine (W)
+    pub heat_input: f64,
+
+    /// Total heat rejection from the engine (W)
+    pub heat_rejection: f64,
+
+    /// Indicated power, assuming no hxr pressure drop (W)
+    pub indicated_power_ideal: f64,
+
+    /// Indicated power, accounting for hxr pressure drop (W)
+    pub indicated_power: f64,
+
+    /// Shaft power (W)
+    ///
+    /// Shaft power is defined as the indicated power less any mechanical
+    /// parasitics in the working spaces.
+    pub shaft_power: f64,
+
+    /// Shaft torque (N-m)
+    pub shaft_torque: f64,
+
+    /// Net power (W)
+    ///
+    /// Net power is defined as the shaft power less any mechanical parasitics
+    /// in the heat exchangers.
+    pub net_power: f64,
+
+    /// Engine efficiency, ignoring electrical parasitics (-)
+    pub eta_mechanical: f64,
+
+    /// Engine efficiency, including electrical parasitics (-)
+    pub eta_overall: f64,
+}
+
+/// Time-discretized values over one engine cycle
 ///
-/// The properties with type `Vec<f64>` represent discrete values in time
-/// with a common index.  For example, at `time[5]` the compression space
-/// temprerature is `T_c[5]` and the mass flow rate between the chx and the
-/// regenerator is `m_dot_kr[5]`.
+/// This struct represents discrete values in time during a single engine
+/// cycle, all sharing a common index.  For example, at `time[5]` the
+/// compression space temperature is `T_c[5]` and the mass flow rate between
+/// the chx and the regenerator is `m_dot_kr[5]`.
 ///
 /// For mass flow rate values, positive values indicate flow from cold to hot
 /// (i.e., positive flow is comp -> chx -> regen -> hhx -> exp).  Negative
 /// values represent mass flow in the opposite direction.
 #[allow(non_snake_case)]
-pub struct RunResults {
+pub struct Values {
     /// Time in the cycle for each discrete point (s)
     pub time: Vec<f64>,
 
@@ -56,52 +110,4 @@ pub struct RunResults {
     ///
     /// Positive values represent heat flow from the heat exchanger to the fluid.
     pub Q_dot_l: Vec<f64>,
-
-    /// Engine pressure (Pa)
-    pub pres: state::Pressure,
-
-    /// Engine temperatures (K)
-    pub temp: state::Temperatures,
-
-    /// Average mass flow rates through the heat exchangers (kg/s)
-    pub mass_flow: state::MassFlows,
-
-    /// Average heat flow rates through the heat exhangers (W)
-    pub heat_flow: state::HeatFlows,
-
-    /// Regenerator approach temperature imbalance (K)
-    pub regen_imbalance: state::RegenImbalance,
-
-    /// Total heat input to the engine (W)
-    pub heat_input: f64,
-
-    /// Total heat rejection from the engine (W)
-    pub heat_rejection: f64,
-
-    /// Indicated power, assuming no hxr pressure drop (W)
-    pub indicated_power_ideal: f64,
-
-    /// Indicated power, accounting for hxr pressure drop (W)
-    pub indicated_power: f64,
-
-    /// Shaft power (W)
-    ///
-    /// Shaft power is defined as the indicated power less any mechanical
-    /// parasitics in the working spaces.
-    pub shaft_power: f64,
-
-    /// Shaft torque (N-m)
-    pub shaft_torque: f64,
-
-    /// Net power (W)
-    ///
-    /// Net power is defined as the shaft power less any mechanical parasitics
-    /// in the heat exchangers.
-    pub net_power: f64,
-
-    /// Engine efficiency, ignoring electrical parasitics (-)
-    pub eta_mechanical: f64,
-
-    /// Engine efficiency, including electrical parasitics (-)
-    pub eta_overall: f64,
 }
