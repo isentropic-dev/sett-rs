e8f300ba3d63a466c0459f8868f4e1f8c6cfc79f
Greg Troszak <gdtroszak@gmail.com>
2023-03-08 11:39:52 -0500

    WS

--- src/ws/mod2.rs
+++ src/ws/mod2.rs
@@ -1,12 +1,31 @@
 use serde::Deserialize;
 
+use crate::types::Material;
+
 use super::{CompVolume, ExpVolume, Parasitics, State, ThermalResistance, WorkingSpaces};
 
 pub struct Mod2 {}
 
 #[allow(non_snake_case)]
-#[derive(Debug, Deserialize, PartialEq, Eq)]
-pub struct Config {}
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct Config {
+    frequency: f64,
+    phaseAngle: f64,
+    D: f64,
+    h: f64,
+    L: f64,
+    stroke: f64,
+    V_clearance_c: f64,
+    R_c: f64,
+    V_clearance_e: f64,
+    R_e: f64,
+    material_p: Material,
+    material_c: Material,
+    th_pw: f64,
+    th_cw: f64,
+    L_cond: f64,
+    e: f64,
+}
 
 impl WorkingSpaces for Mod2 {
     fn frequency(&self, _state: &State) -> f64 {
