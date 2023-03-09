90abf9bed588dd4d5972501ba3e0c149896b15bd
John Dyreby <john@nnumerics.com>
2023-03-09 10:02:47 -0600

    Add config reading and engine running

--- matlab/src/lib.rs
+++ matlab/src/lib.rs
@@ -1,25 +1,79 @@
 #![cfg(target_os = "windows")]
 #![allow(dead_code)]
 
-use std::ffi::CString;
-use std::os::raw::{c_char, c_int};
+use std::ffi::{c_char, c_int, CStr, CString};
+
+use config::ConfigError;
 
 #[no_mangle]
 pub extern "C" fn mexFunction(
     nlhs: c_int,
     _plhs: *mut *mut mxArray,
     nrhs: c_int,
-    _prhs: *const *const mxArray,
+    prhs: *const *const mxArray,
 ) {
     // Check that the number of inputs and outputs are correct
     if nrhs != 1 {
         matlab_error(
-            "The sett function must be called with one input: sett(\"path_to_config.toml\")",
+            "The sett function must be called with one input: sett('path_to_config.toml')",
         );
     }
     if nlhs > 1 {
         matlab_error("A single struct is returned");
     }
+
+    // Read the config file
+    let config = match config_from_rhs_arg(prhs) {
+        Ok(config) => config,
+        Err(message) => {
+            matlab_error(message);
+            return;
+        }
+    };
+
+    // Run the engine
+    sett::run_from_config(config);
+
+    // TODO:
+    // 1) Check that prhs arg is char array and not string
+    // 2) Modify `run_from_config` to return `RunResults`
+    // 3) Create and fill MATLAB struct for `RunResults`
+
+    matlab_error("things worked!");
+}
+
+/// Attempt to read the config file provided as MATLAB argument
+///
+/// If anything goes wrong, this function returns a `&'static str` with an
+/// error message intended to be sent to MATLAB.
+fn config_from_rhs_arg(prhs: *const *const mxArray) -> Result<sett::Config, &'static str> {
+    let file_path = unsafe {
+        let str_ptr = mxArrayToString(*prhs);
+        match CStr::from_ptr(str_ptr).to_str() {
+            Ok(str) => str,
+            Err(_) => {
+                return Err("unknown error"); // unlikely to ever happen
+            }
+        }
+    };
+
+    let config = match config::Config::builder()
+        .add_source(config::File::with_name(file_path))
+        .build()
+    {
+        Ok(config) => config,
+        Err(err) => {
+            let error = match err {
+                ConfigError::NotFound(_) => "file not found",
+                _ => "unknown config error",
+            };
+            return Err(error);
+        }
+    };
+
+    config
+        .try_deserialize::<sett::Config>()
+        .map_err(|_| "invalid config file")
 }
 
 #[repr(C)]
@@ -58,10 +112,11 @@ extern "C" {
         fieldnames: *const *const c_char,
     ) -> *mut mxArray;
     fn mxSetFieldByNumber(pm: *mut mxArray, index: usize, fieldnumber: i32, pvalue: *mut mxArray);
+    fn mxArrayToString(pa: *const mxArray) -> *const c_char;
 }
 
 fn matlab_error(msg: &str) {
-    let msg = CString::new(msg).expect("CString::new failed");
+    let msg = CString::new(msg).expect("CString::new should never fail");
     unsafe {
         mexErrMsgTxt(msg.as_ptr());
     }
